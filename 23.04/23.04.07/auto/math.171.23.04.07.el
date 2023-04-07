ec7dd17de1c90671e69b202e2f21c0b19ef51311
Kyle Dituro <kmd0612@gmail.com>
2023-04-07 10:32:43 -0400

    Added notes for april 7th

---
+++ 23.04/23.04.07/auto/math.171.23.04.07.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "math.171.23.04.07"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt" "twosided")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "sdpreamble"
+    "article"
+    "art12"))
+ :latex)
+
